508317ae618a9404ef285134566e74d509b1fd62
Maria Fernanda Aurelio <fer.aurelio@hotmail.com>
2019-08-19 16:00:55 -0300

    subo logo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,8 @@
 
 <section class="container-section">
 
-  <h1 class="h1-index"><strong>Sabiondos</strong></h1>
-
+  <!--<h1 class="h1-index"><strong>Sabiondos</strong></h1>-->
+ <img src="/img/logoSabiondos.png" style="width:280px; margin-top:30px;" alt="Logo Sabiondos">
 <h2 class="sub-ppal">Un juego para despertar tu curiosidad</h2>
 
  <div class="image-mobile">
