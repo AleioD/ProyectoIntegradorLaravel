04cd4d30ae3e5cbbc0794e927dce104c5789085d
AleioD <alejandrodaquila@hotmail.com>
2019-08-20 21:30:24 -0300

    arreglo de migration de user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,18 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('userName');
+            $table->string('userName')->unique();
             $table->string('name');
             $table->string('surname');
             $table->string('country');
-            $table->string('state');
+            $table->string('state')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('avatar');
-            $table->integer('score');
-            $table->boolean('isAdmin');
-            $table->boolean('isActive');
+            $table->integer('score')->nullable();
+            $table->boolean('isAdmin')->nullable()->default(0);
+            $table->boolean('isActive')->nullable()->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
