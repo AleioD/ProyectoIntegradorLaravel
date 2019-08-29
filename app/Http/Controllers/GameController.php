ee38350a263bd8d782ad23dac2c1ee1d632278b5
CatalinaPicco <catalina.picco@gmail.com>
2019-08-28 21:39:51 -0300

    descomente el metodo de empezar juego

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -19,14 +19,14 @@ class GameController extends Controller
         return view('category');
     }
 
-    /*public function startGame()
+    public function startGame()
     {
       $puntos = $this->score;
       $question = Question::inRandomOrder()->first();
       $id = $question->getId();
       $answers = Answer::inRandomOrder()->where('question_id', $id)->get();
       return view('gameplay', compact('question', 'id', 'answers', 'puntos'));
-    }*/
+    }
 
     public function select(Request $form) {
       $puntos = $this->score;
